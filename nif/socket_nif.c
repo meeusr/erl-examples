54da7295934609e2f16b52a2f8b9baf75f26c599
Ronny.meeus@gmail.com <Ronny.meeus@gmail.com>
2012-08-14 16:20:35 +0200

    Extended nif with UDP tests

--- nif/socket_nif.c
+++ nif/socket_nif.c
@@ -1,31 +1,44 @@
 #include "erl_nif.h"
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <stdio.h>
 
 static ERL_NIF_TERM sock_socket_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 {
-    int port, sock;
-    struct sockaddr_in sock_addr;
-
-    if (!enif_get_int(env, argv[0], &port)) {
-	return enif_make_badarg(env);
-    }
-
+    int sock;
     sock = socket(AF_INET,SOCK_DGRAM,IPPROTO_UDP);
-
-    sock_addr.sin_family = AF_INET;
-    sock_addr.sin_port = htons(port);
-    sock_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    if (bind(sock,(struct sockaddr*)&sock_addr,sizeof(sock_addr)) != 0) {
-	close(sock);
+    if (sock < 0) {
 	return enif_make_badarg(env);
     }
     return enif_make_int(env, sock);
 }
 
+
+static ERL_NIF_TERM sock_bind_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+{
+    int port, sock;
+    struct sockaddr_in sock_addr;
+
+    if (!enif_get_int(env, argv[0], &sock)) {
+        return enif_make_badarg(env);
+    }
+    if (!enif_get_int(env, argv[1], &port)) {
+        return enif_make_badarg(env);
+    }
+
+    sock_addr.sin_family = AF_INET;
+    sock_addr.sin_port = htons(port);
+    sock_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    if (bind(sock,(struct sockaddr*)&sock_addr,sizeof(sock_addr)) != 0) {
+        return enif_make_badarg(env);
+    }
+    return enif_make_int(env, sock);
+
+}
+
 static ERL_NIF_TERM sock_send_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 {
-    int sock;
+    int sock,dest_port,ret;
     struct sockaddr_in sock_addr;
     char *data = "TEST\n";
     struct sockaddr_in destination;
@@ -33,20 +46,24 @@ static ERL_NIF_TERM sock_send_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM a
     if (!enif_get_int(env, argv[0], &sock)) {
         return enif_make_badarg(env);
     }
+    if (!enif_get_int(env, argv[1], &dest_port)) {
+        return enif_make_badarg(env);
+    }
 
     destination.sin_family = AF_INET;
-    destination.sin_port = htons(9000);
+    destination.sin_port = htons(dest_port);
     inet_pton(AF_INET,"127.0.0.1",&destination.sin_addr.s_addr);
-    sendto(sock,data,5,0,(struct sockaddr*)&destination,sizeof(destination));
-
-    return enif_make_int(env, sock);
+    ret = sendto(sock,data,5,0,(struct sockaddr*)&destination,sizeof(destination));
+    if (ret <= 0) {
+        return enif_make_badarg(env);
+    }
+    return enif_make_int(env, ret);
 }
 
 static ERL_NIF_TERM sock_loopback_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 {
     int ret,sock;
     socklen_t addr_len;
-    struct sockaddr_in sock_addr;
     char data[2000];
     struct sockaddr_in source;
     
@@ -54,8 +71,9 @@ static ERL_NIF_TERM sock_loopback_nif(ErlNifEnv* env, int argc, const ERL_NIF_TE
         return enif_make_badarg(env);
     }
     
-    recvfrom(sock,data,sizeof(data),0,(struct sockaddr*)&source,&addr_len);
-    ret = sendto(sock,data,5,0,(struct sockaddr*)&source,addr_len);
+    ret = recvfrom(sock,data,sizeof(data),0,(struct sockaddr*)&source,&addr_len);
+//    printf("Data received %d\n",ret);
+    ret = sendto(sock,data,ret,0,(struct sockaddr*)&source,addr_len);
 
     return enif_make_int(env, ret);
 }
@@ -72,8 +90,9 @@ static ERL_NIF_TERM sock_close_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM 
 }
 
 static ErlNifFunc nif_funcs[] = {
-    {"socket", 1, sock_socket_nif},
-    {"send", 1, sock_send_nif},
+    {"socket", 0, sock_socket_nif},
+    {"bind", 2, sock_bind_nif},
+    {"send", 2, sock_send_nif},
     {"loopback", 1, sock_loopback_nif},
     {"close", 1, sock_close_nif}
 };
--- nif/socket_nif.erl
+++ nif/socket_nif.erl
@@ -1,18 +1,38 @@
 -module(socket_nif).
--export([socket/1, send/1,loopback/1, close/1]).
+-export([socket/0, bind/2, send/2 ,loopback/1, close/1, create_server/1, client_sync/2]).
 -on_load(init/0).
 
 init() ->
     ok = erlang:load_nif("./socket_nif", 0).
 
-close(_X) ->
+socket() ->
     exit(nif_library_not_loaded).
-socket(_Y) ->
+bind(_SOCK,_Port) ->
     exit(nif_library_not_loaded).
-send(_X) ->
+send(_SOCK,_DestPort) ->
     exit(nif_library_not_loaded).
-loopback(_X) ->
+loopback(_SOCK) ->
     exit(nif_library_not_loaded).
+close(_SOCK) ->
+    exit(nif_library_not_loaded).
+
+create_server(Port) ->
+    SRV = socket_nif:socket(),
+    socket_nif:bind(SRV,Port),
+    register(udp_server,spawn(fun() -> server_loop(SRV) end)).
+
+server_loop(Sock) ->
+   socket_nif:loopback(Sock),
+   server_loop(Sock).
+
+client_sync(N,Port) ->
+   CLT = socket_nif:socket(),
+   client_loop(CLT,Port,N).
+
+client_loop(_Sockn,_Port,0) -> ok;
+client_loop(Sock,Port,N) ->
+   socket_nif:send(Sock,Port),
+   client_loop(Sock,Port,N-1).
 
 
 
